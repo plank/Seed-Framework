ba4cde4acb550cbad3323fca0b8ae52454cfbf7b
mateo_at_plank <mateo_at_plank@e74265ba-5712-0410-b544-ed03b9d7f503>
2006-05-02 22:10:35 +0000

    Updated tests for URL class.
    git-svn-id: https://seedframework.svn.sourceforge.net/svnroot/seedframework/trunk@9 e74265ba-5712-0410-b544-ed03b9d7f503

--- tests/classes/url_test.php
+++ tests/classes/url_test.php
@@ -47,6 +47,26 @@ class UrlTester extends UnitTestCase {
 		$url = new URL('../../../hello.php', $base_href);
 		$this->assertEqual($url->to_string(), 'http://www.plankdesign.com/hello.php');
 		
+		// make sure we can properly ignore query strings
+		$url = new URL('some_new_place?id=1', $base_href);
+		$this->assertEqual($url->to_string(false), 'http://www.plankdesign.com/test/path/some_new_place');
+		
+	}
+	
+	function test_query_string() {
+		$url = new URL('http://www.plankdesign.com/index.php?id=2&page=3')	;
+		$this->assertEqual($url->query, 'id=2&page=3');
+		$this->assertEqual($url->query_array, array('id'=>'2','page'=>'3'));
+		$this->assertEqual($url->to_string(), 'http://www.plankdesign.com/index.php?id=2&page=3');
+		
+		// we can choose which parts of the query string are included in the string, as well as their order
+		$this->assertEqual($url->to_string('id'), 'http://www.plankdesign.com/index.php?id=2');
+		$this->assertEqual($url->to_string('page'), 'http://www.plankdesign.com/index.php?page=3');
+		$this->assertEqual($url->to_string(array('page', 'id')), 'http://www.plankdesign.com/index.php?page=3&id=2');
+		
+		// unfound query string parts are ignored
+		$this->assertEqual($url->to_string('dummy'), 'http://www.plankdesign.com/index.php');
+		
 	}
 	
 }
