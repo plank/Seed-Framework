565f1c29b9871e9c44e99e6c8a9563a1956c0814
mateo_at_plank <mateo_at_plank@e74265ba-5712-0410-b544-ed03b9d7f503>
2006-05-23 19:41:09 +0000

    Add logger include. Moved extension ignoring config into class to avoid having to load libraries if we want to ignore request. Added detection of index files for static directory requests. Moved output file functionality to its own method, with support for php files. Removed die()s and replaced with returns. Added comments to defines.
    git-svn-id: https://seedframework.svn.sourceforge.net/svnroot/seedframework/trunk@37 e74265ba-5712-0410-b544-ed03b9d7f503

--- framework.php
+++ framework.php
@@ -15,6 +15,10 @@
  */
 require_once('classes/file.php');
 
+/**
+ * Include logger for dispatch logging
+ */
+require_once('classes/logger.php');
 
 /**
  * shortcut to app startup
@@ -28,35 +32,168 @@ function start() {
 
 /**
  * Application class
+ *
+ * This class handles the requests, and loads Call the start() method to start execution. 
  */
 class Seed {
 
 	/**
 	 * All the files in each of these subfolders will be included
+	 *
+	 * @var array
 	 */
 	var $subfolders = array('classes', 'libs', 'view', 'model', 'controller');
 	
+	/**
+	 * A comma delimited list of file extensions the framework shouldn't handle 
+	 *
+	 * @var string
+	 */
+	var $ignore_extensions = 'js,css,jpg,jpeg,gif,png,flw';
+	
+	/**
+	 * Turn on check for static files in the "public" directory. This is required
+	 * when full mod_rewrite support is missing.
+	 *
+	 * @var bool
+	 */
 	var $handle_static_files = false;
 	
+	/**
+	 * These are the various file names an index file can take
+	 *
+	 * @var array
+	 */
+	var $index_file_names = array('index', 'default', 'welcome');
+	
+	/**
+	 * These are the various file extensions an index file can take
+	 *
+	 * @var array
+	 */
+	var $index_file_extensions = array('php', 'html', 'htm');
+	
+	/**
+	 * Call this function to start processing
+	 *
+	 * @return bool Returns true
+	 */
+	function start() {
+		$url = isset($_GET['url']) ? $_GET['url']: '';
+		
+		// if the $url points to a static file in the public folder, display that
+		if ($this->display_static_files($url)) {
+			return true;	
+		}
+
+		// ignore requests with extensions defined in IGNORE_EXTENSIONS
+		if ($this->ignore_extensions($url)) {
+			return true;		
+		}
+
+		// include all the framework libraries		
+		$this->include_libraries();
+		
+		// include the app's general config file
+		$this->include_config();
+	
+		// include the app's environment config file
+		$this->include_environment_config();
+
+		// include application files
+		$this->include_application_files();
+		
+		// register all objects
+		$this->register_objects();
+		
+		// log the request
+		Logger::log('dispatch', LOG_LEVEL_DEBUG, 'dispatching '. $url);
+		
+		// dispatch the request
+		Dispatcher::dispatch();
+		
+		return true;
+		
+	}	
+	
+	/**
+	 * Checks for the existance of a static file in the "public" directory at the given url
+	 * and outputs that to the browser if a file exists there.
+	 *
+	 * @param string $url The url of the file to handle
+	 * @return bool Returns true if a file was output, false if not
+	 */
 	function display_static_files($url) {
 		if (!$this->handle_static_files || !$url) {
-			return true;	
+			return false;	
 		}
 		
-		$file = new File(PUBLIC_ROOT_PATH.$url);
+		$file = new File(PUBLIC_PATH.$url);
 		
 		if (!$file->exists()) {
-			return true;	
+			return false;	
 		}
 		
-		$mimetype = $file->get_mime_type();
-		header("HTTP/1.0 200 OK", true, 200);
-		header("Content-type: $mimetype");
-
-		$file->output_contents();
+		if ($file->is_directory()) {
+			$file = $this->get_directory_index($file);
 			
-		die();
+		}
 		
+		if ($file) {
+			Logger::log('dispatch', LOG_LEVEL_DEBUG, 'static '. $url);
+			
+			return $this->output_file($file);
+			
+		} else {
+			return false;
+			
+		}
+		
+	}
+	
+	/**
+	 * Checks a given directory for the existance of an index file
+	 *
+	 * @param File $file
+	 * @return File Returns a file object representing the index file if there was one found, return false if not.
+	 */
+	function get_directory_index($file) {
+		// try all permutations of file names and extensions.
+		foreach ($this->index_file_names as $name) {
+			foreach ($this->index_file_extensions as $extension) {
+				$index_file = new File($file->path.'/'.$name.'.'.$extension);
+				
+				if ($index_file->exists()) {
+					return $index_file;	
+				}
+			}	
+		}			
+
+		return false;
+		
+	}
+	
+	/**
+	 * Output the file to the browser with proper headers
+	 *
+	 * @param File $file
+	 * @return bool Returns true.
+	 */
+	function output_file($file) {
+		// if the file is a php file, include it
+		if ($file->get_extension() == 'php') {
+			include($file->get_path());
+				
+		} else {
+			$mimetype = $file->get_mime_type();
+			header("HTTP/1.0 200 OK", true, 200);
+			header("Content-type: $mimetype");
+	
+			$file->output_contents();
+			
+		}
+
+		return true;
 	}
 	
 	/**
@@ -100,26 +237,34 @@ class Seed {
 	 * Check the requested url to make sure it's not on the ignored extensions list
 	 *
 	 * @param string $url The requested url
+	 * @return bool Returns true if $url is ignores
 	 */
 	function ignore_extensions($url) {
 		// Ignore extensions, if configured
-		if (defined('IGNORE_EXTENSIONS') && IGNORE_EXTENSIONS) {
-			$ignore = str_replace(',', '|', IGNORE_EXTENSIONS);
+		if (!$this->ignore_extensions) {
+			return false;
+				
+		}
+			
+		$ignore = str_replace(',', '|', $this->ignore_extensions);
 	
-			if (preg_match('/\.('.$ignore.')$/i', $url)) {	
-				Logger::log('dispatch', LOG_LEVEL_DEBUG, 'ignoring '. $url);
-				header("HTTP/1.0 404 Not Found", true, 404);
-				die('page not found');
-			}
-		}		
+		if (preg_match('/\.('.$ignore.')$/i', $url)) {	
+			Logger::log('dispatch', LOG_LEVEL_DEBUG, 'ignoring '. $url);
+			header("HTTP/1.0 404 Not Found", true, 404);
+			print ('page not found');
+			return true;
+			
+		} else {
+			return false;
+			
+		}
 		
 	}
-	
+
+	/**
+	 * Include all the libraries
+	 */
 	function include_libraries() {
-		
-		/**
-		 * Include all the files in each subfolder
-		 */
 		foreach($this->subfolders as $subfolder) {
 			$path = FRAMEWORK_PATH.$subfolder.'/';
 		
@@ -128,6 +273,7 @@ class Seed {
 		}		
 		
 	}
+	
 	
 	function include_application_files() {
 
@@ -142,47 +288,13 @@ class Seed {
 		
 	}
 	
-	/**
-	 * Call this function to start processing
-	 */
-	function start() {
-		$url = isset($_GET['url']) ? $_GET['url']: '';
-		
-		// if the $url points to a static file in the public folder, display that
-		$this->display_static_files($url);
-		
-		// include the app's general config file
-		$this->include_config();
-	
-		// include the app's environment config file
-		$this->include_environment_config();
-		
-		// ignore requests with extensions defined in IGNORE_EXTENSIONS
-		$this->ignore_extensions($url);
-		
-		// include all the framework libraries		
-		$this->include_libraries();
 
-		// include application files
-		$this->include_application_files();
-		
-		// register all objects
-		$this->register_objects();
-		
-		// log the request
-		Logger::log('dispatch', LOG_LEVEL_DEBUG, 'dispatching '. $url);
-		
-		// dispatch the request
-		Dispatcher::dispatch();
-		
-	}
 
 	/**
 	 * Includes all the files in a given dir
 	 *
 	 * @param string $path
 	 */
-	
 	function require_dir($path) {
 		$dir = dir($path);
 		
@@ -195,9 +307,7 @@ class Seed {
 				require_once($path.$file);
 			}
 		}
-		
 	}	
-	
 }
 
 /**
@@ -205,9 +315,8 @@ class Seed {
  */
 $default_values = array();
 
-/**
- * Define the defaults
- */
+// Define the defaults
+ 
 foreach($default_values as $name => $value) {
 	if (!defined($name)) {
 		define($name, $value);
@@ -215,24 +324,70 @@ foreach($default_values as $name => $value) {
 }
 
 
+// Framework path constants
+
 /**
- * Path constants
+ * Path to the framework
  */
 define('FRAMEWORK_PATH', dirname(__FILE__).'/');
 
-define('APP_PATH', dirname(dirname($_SERVER['PATH_TRANSLATED'])).'/');	
-define('CONTROLLER_PATH', APP_PATH.'app/controllers/');
-define('TEMPLATE_PATH', APP_PATH.'app/views/');
-define('MODEL_PATH', APP_PATH.'app/models/');
-define('HELPER_PATH', APP_PATH.'app/helpers/');
-define('CONFIG_PATH', APP_PATH.'config/');
-define('PUBLIC_ROOT_PATH', APP_PATH.'public/');
-define('VENDOR_PATH', APP_PATH.'vendor/');
+/**
+ * Path to the root of the framework templates. This is where the scaffold template are located.
+ */
 define('FRAMEWORK_TEMPLATE_PATH', FRAMEWORK_PATH.'templates/');
-define('LOG_PATH', APP_PATH.'logs/');
+
+
+// Application path constants
 
 /**
- * Url constants
+ * Path to the root of the application
+ */
+define('APP_PATH', dirname(dirname($_SERVER['PATH_TRANSLATED'])).'/');	
+
+/**
+ * Path to the root of controllers
+ */
+define('CONTROLLER_PATH', APP_PATH.'app/controllers/');
+
+/**
+ * Path to the root of the templates
+ */
+define('TEMPLATE_PATH', APP_PATH.'app/views/');
+
+/**
+ * Path to the root of the models
+ */
+define('MODEL_PATH', APP_PATH.'app/models/');
+
+/**
+ * Path to the root of the helpers
+ */
+define('HELPER_PATH', APP_PATH.'app/helpers/');
+
+/**
+ * Path to the root of the config files
+ */
+define('CONFIG_PATH', APP_PATH.'config/');
+
+/**
+ * Path to the root of the public files
+ */
+define('PUBLIC_PATH', APP_PATH.'public/');
+
+/**
+ * Path to the root of the vendor files
+ */
+define('VENDOR_PATH', APP_PATH.'vendor/');
+
+/**
+ * Path to the root of the log
+ */
+define('LOG_PATH', APP_PATH.'logs/');
+
+// Application URL constants
+
+/**
+ * Hostname with protocol
  */
 define('APP_HOST', 'http://'.$_SERVER['HTTP_HOST']);
 
@@ -254,13 +409,11 @@ if (APP_URL == '/') {
 }
 
 define('PUBLIC_ROOT', APP_ROOT.'public/');
+
+/**
+ * The current request URI
+ */
 define('REQUEST_URI', $_SERVER['REQUEST_URI']);
-
-
-
-
-
-
 
 if (defined('SESSION_LIFETIME')) {
 	ini_set('session.gc_maxlifetime', SESSION_LIFETIME);
