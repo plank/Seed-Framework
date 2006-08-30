00acda67b08d4898b07276f32b086afa1d67b7a8
mateo_at_plank <mateo_at_plank@e74265ba-5712-0410-b544-ed03b9d7f503>
2006-08-30 18:47:46 +0000

    Made postgres tests optional.
    git-svn-id: https://seedframework.svn.sourceforge.net/svnroot/seedframework/trunk@180 e74265ba-5712-0410-b544-ed03b9d7f503

--- db/test/pgsql_db_test.php
+++ db/test/pgsql_db_test.php
@@ -9,35 +9,63 @@ class PgsqlDBTester extends UnitTestCase {
 	var $db;
 	
 	function setup() {
+		if (SKIP_PG_TESTS) {
+			return ;
+		}	
+		
 		$this->db = DB::factory('pgsql', 'localhost', '', '', 'unit_tests');
 		
 	}
 	
 	function test_creation() {
+		if (SKIP_PG_TESTS) {
+			return ;
+		}	
+		
 		$this->assertIsA($this->db, 'PgsqlDB');
 		
 	}
 	
 	function test_simple_query() {
+		if (SKIP_PG_TESTS) {
+			return ;
+		}	
+		
 		$result = $this->db->query('select * from test');	
 		$this->assertTrue($result);
 		
 	}
 	
 	function test_query_value() {
+		if (SKIP_PG_TESTS) {
+			return ;
+		}	
+		
 		$this->assertEqual($this->db->query_value('select id from test'), array(1, 2));
 		
 	}
 	
 	function test_query_array() {
+		if (SKIP_PG_TESTS) {
+			return ;
+		}	
+		
 		$this->assertEqual($this->db->query_array('select * from test limit 1'), array(array('id'=>1, 'title'=>'One', 'text'=>'Text one')));
 	}
 	
 	function test_query_single() {
+		if (SKIP_PG_TESTS) {
+			return ;
+		}	
+		
 		//$this->dump($this->db->query_single('select * from test'));		
 	}
 
 	function test_columns() {
+		if (SKIP_PG_TESTS) {
+			return ;
+		}	
+		
 		$columns = $this->db->columns('test');
 		
 		$this->assertEqual($columns['id']->name, 'id');
