141a5b85ac4f2242bf9ad3acab347072c91b771a
mateo_at_plank <mateo_at_plank@e74265ba-5712-0410-b544-ed03b9d7f503>
2006-09-01 15:19:13 +0000

    Removed hardcoded seed_model_version.
    git-svn-id: https://seedframework.svn.sourceforge.net/svnroot/seedframework/trunk@186 e74265ba-5712-0410-b544-ed03b9d7f503

--- model/lib/model.php
+++ model/lib/model.php
@@ -1,8 +1,5 @@
 <?php
 
-
-define('SEED_MODEL_VERSION', 2);
-
 if (SEED_PHP_VERSION == 4 && (defined('SEED_MODEL_VERSION') && SEED_MODEL_VERSION == 1)) {
 	require_once('model_versions/version1.php');
 	
