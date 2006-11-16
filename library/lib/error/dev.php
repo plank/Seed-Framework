6690a8b8b5750b4c6d7d87d3f305f8240180b721
mateo_at_plank <mateo_at_plank@e74265ba-5712-0410-b544-ed03b9d7f503>
2006-11-16 22:16:27 +0000

    Update to dev error handler.
    git-svn-id: https://seedframework.svn.sourceforge.net/svnroot/seedframework/trunk@270 e74265ba-5712-0410-b544-ed03b9d7f503

--- library/lib/error/dev.php
+++ library/lib/error/dev.php
@@ -1,5 +1,38 @@
 <?php
 
+
+
+function display_source($source, $errline = 0, $extracted_lines = 0) {
+	
+	if ($extracted_lines) {
+		$number = $errline > $extracted_lines ? $errline - $extracted_lines : 1;
+		
+		$source = array_splice($source, $number, $extracted_lines * 2 - 1);
+	}
+	
+	print "<ol>\n";
+	
+	foreach ($source as $line) {
+		$number ++;
+		
+		$line = str_replace("\r", '', $line);
+		$line = str_replace("\n", '', $line);
+		
+		if (!trim($line)) {
+			print "<li value='$number'>&nbsp;</li>\n";
+			
+		} else if ($number == $errline) {
+			print "<li value='$number' id='error_line'><pre style='display: inline'>".htmlentities($line)."</pre></li>\n";
+			
+		} else {
+			print "<li value='$number'><pre style='display: inline'>".htmlentities($line)."</pre></li>\n";
+			
+		}
+	}	
+	
+	print "</ol>\n";	
+}
+
 /**
  * Error handler for development
  */
@@ -17,6 +50,7 @@ function error_handler($errno, $errstr, $errfile, $errline, $errcontext)
 	
 	// clean all buffers
 	ob_end_clean_all();
+	
 	print "<html><head><title>Seed - ".error_string($errno)."</title>";
 	print "<script type='text/javascript' src='/system/tabber.js'></script>";
 	print "<link href='/system/error.css' rel='Stylesheet' type='text/css' /></head>";
@@ -24,37 +58,19 @@ function error_handler($errno, $errstr, $errfile, $errline, $errcontext)
 	print "<body>";
 	print "<h1>".ucfirst(error_string($errno)).": $errstr</h1>";
 	
-	print "<p>$errfile - <a href='#error_line'>line $errline</a></p>";
+	print "<p>in <i>$errfile</i> - line <b>$errline</b></p>";
+
+	$source = explode("\n", file_get_contents($errfile));
+	
+	display_source($source, $errline, 4);
 	
 	print "<div class='tabber'>";
+
+	
 	print "<div class='tabbertab'>";
 	print "<h2>Backtrace</h2>";
 	print(nl2br(backtrace(3)));
-	print "</div>";
-	
-	print "<div class='tabbertab'>";
-	print "<h2>Source</h2>";
-	
-	$source = explode("\n", file_get_contents($errfile));
-	
-	print "<ol>\n";
-	
-	foreach ($source as $number => $line) {
-		$number ++;
-		
-		if (!$line) {
-			$line = ' ';	
-		}
-		
-		if ($number == $errline) {
-			print "<li id='error_line'>".highlight_string($line, true)."</li>\n";	
-		} else {
-			print "<li>".highlight_string($line, true)."</li>\n";
-		}
-	}
-	
-	print "</ol>\n";
-	print "</div>";
+	print "</div>";	
 	
 	foreach(array('Get'=>$_GET, 'Post'=>$_POST, 'Cookies'=>$_COOKIE, 'Session'=>$_SESSION, 'Server'=>$_SERVER, 'Env'=>$_ENV, 'Context'=>$errcontext) as $title => $vars) {
 		print "<div class='tabbertab'>";
