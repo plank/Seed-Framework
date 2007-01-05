48b0b1a951a76e30e30e90943f96b3e798f3018c
mateo_at_plank <mateo_at_plank@e74265ba-5712-0410-b544-ed03b9d7f503>
2007-01-05 19:19:02 +0000

    Finished preliminary support for single argument options with tests.
    git-svn-id: https://seedframework.svn.sourceforge.net/svnroot/seedframework/trunk@312 e74265ba-5712-0410-b544-ed03b9d7f503

--- cli/lib/cliargs.php
+++ cli/lib/cliargs.php
@@ -10,10 +10,49 @@ class CLIArgs {
 	/**
 	 * @var array
 	 */
-	var $args;
+	var $params;
+
+	/**
+	 * @var array
+	 */
+	var $short_options;
 	
-	function CLIArgs() {
-		list($this->args, $this->options) = $this->parse_args($this->get_args());
+	/**
+	 * @var array
+	 */
+	var $long_options;
+	
+	/**
+	 * @var bool
+	 */
+	var $valid;
+	
+	/**
+	 * Constructor
+	 *
+	 * @param array $args
+	 * @param string $short_options
+	 * @param array $long_options
+	 * @return CLIArgs
+	 */
+	function CLIArgs($args = null, $short_options = null, $long_options = null) {
+		if (is_null($args)) {
+			$this->params = $this->get_args();	
+		} elseif (is_string($args)) {
+			$this->params = preg_split('/[[:space:]]+/', $args);
+		} elseif (is_array($args)) {
+			$this->params = $args;	
+		}
+
+		if (is_string($short_options)) {
+			$this->short_options = $this->parse_short_options($short_options);	
+		} elseif (is_array($short_options)) {
+			$this->short_options = $short_options;	
+		}
+		
+		$this->options = array();
+		
+		$this->valid = $this->extract_args();
 	}
 	
 	/**
@@ -35,24 +74,127 @@ class CLIArgs {
 		
 	}
 	
-	function parse_args($args, $short_options, $long_options = null) {
-		if (!is_array($args)) {
-			$args = split('[[:space:]]+', $args);			
+	/**
+	 * Extracts options from params according to the given rules
+	 *
+	 * @return bool
+	 */
+	function extract_args() {
+
+		while(!is_null($arg = array_shift($this->params))) {
+
+			// explicit end of options
+			if (is_null($arg) || $arg == '--') {
+				return true;	
+			}
+			
+			// end of options
+			if (substr($arg, 0, 1) != '-' && substr($arg, 0, 2) != '--') {
+				array_unshift($this->params, $arg);	
+				return true;
+			}
+			
+			// long option
+			if (substr($arg, 0, 2) == '--') {
+				$result = $this->extract_long_option(substr($arg, 2));
+				
+			} else {
+				$result = $this->extract_short_option(substr($arg, 1));	
+
+			}
+			
+			// was there an error?
+			if (!$result) {
+				return false;	
+			}
+			
 		}
 		
-		foreach ($args as $index => $arg) {
-			if (substr($arg, 0, 1) == '-') {
-				$result = CLIArgs::parse_short_options($arg, $short_options);
-			}
+		return true;
+		
+	}
+	
+	/**
+	 * @param string $string
+	 * @return array
+	 */
+	function parse_short_options($string) {
+		$result = array();
+		
+		preg_match_all('/([A-Za-z0-9])(:{0,2})/', $string, $matches);
+		
+		for($i = 0; $i < count($matches[1]); $i ++) {
+			$result[$matches[1][$i]] = $matches[2][$i];	
 		}
 		
 		return $result;
 		
 	}
 	
-	function parse_short_options($arg, $options) {
+	/**
+	 * @param array $string
+	 * @return array
+	 */	
+	function parse_long_options($array) {
+		$result = array();
+		
+		foreach($array as $element) {
+			preg_match('/([A-Za-z0-9]*)(={0,2})/', $element, $matches);
+			$result[$matches[1]] = $matches[2];
+		}
+		
+		return $result;
+	}
+	
+	/**
+	 * @param string $arg
+	 * @return bool
+	 */
+	function extract_short_option($arg) {
+		for($i = 0; $i < strlen($arg); $i ++) {
+			$option = $arg{$i};
+			
+			// found key isn't in the options
+			if (!key_exists($option, $this->short_options)) {
+				return false;
+			}
+			
+			$specifier = $this->short_options[$option];
+			
+			if ($specifier == ':' || $specifier == '::') {
+				if ($i < strlen($arg) - 1) {
+					// if there are characters left in the argument grab those, if not grab the next param
+					$this->options[$option] = substr($arg, $i + 1);	
+				
+				} elseif (count($this->params) && $specifier == ':') {
+					// if there's a next option grab that, but only if the value is required 
+					// need to confirm that this is this correct behavior
+					$this->options[$option] = array_shift($this->params);	
+					
+				} elseif ($specifier == ':') {
+					// if the specificer is not optional, we've got an error
+					return false;	
+					
+				}
+				
+				return true;
+				
+			} else {
+				$this->options[$option] = true;	
+			}
+		}
+		
+		return true;
+	}
+
+	/**
+	 * @param string $arg
+	 * @return bool
+	 */
+	function extract_long_option($arg) {
 		
 	}
+	
 }
 
 
--- cli/test/cliargs_test.php
+++ cli/test/cliargs_test.php
@@ -3,10 +3,65 @@
 
 class CLIArgsTester extends UnitTestCase {
 
-	
-	function test_parse_args() {
+	function test_short_options_parser() {
+		$this->assertEqual(CLIArgs::parse_short_options('abc'), array('a'=>'', 'b'=>'', 'c'=>''));
+		$this->assertEqual(CLIArgs::parse_short_options('ab:c::'), array('a'=>'', 'b'=>':', 'c'=>'::'));
 		
-		$this->assertEqual(CLIArgs::parse_args('-abc', 'abc'), array('a'=>true, 'b'=>true, 'c'=>true));
+	}
+	
+	function test_long_options_parser() {
+		$this->assertEqual(CLIArgs::parse_long_options(array('stringa', 'stringb=', 'stringc==')), array('stringa'=>'', 'stringb'=>'=', 'stringc'=>'=='));
+		
+	}
+
+	
+	/**
+	 * Copy of the the tests from PEAR::getopt
+	 */
+	function test_pear_tests() {
+		
+		$args = new CLIArgs('-abc', 'abc');
+		$this->assertIdentical($args->options, array('a'=>true, 'b'=>true, 'c'=>true));
+		$this->assertIdentical($args->params, array());
+		$this->assertTrue($args->valid);		
+		
+		$args = new CLIArgs('-abc foo', 'abc');
+		$this->assertIdentical($args->options, array('a'=>true, 'b'=>true, 'c'=>true));
+		$this->assertIdentical($args->params, array('foo'));
+		$this->assertTrue($args->valid);		
+		
+		$args = new CLIArgs('-abc foo', 'abc:');
+		$this->assertIdentical($args->options, array('a'=>true, 'b'=>true, 'c'=>'foo'));
+		$this->assertIdentical($args->params, array());		
+		$this->assertTrue($args->valid);		
+		
+		$args = new CLIArgs("-abc foo bar gazonk", "abc");
+		$this->assertIdentical($args->options, array('a'=>true, 'b'=>true, 'c'=>true));
+		$this->assertIdentical($args->params, array('foo', 'bar', 'gazonk'));		
+		$this->assertTrue($args->valid);		
+		
+		$args = new CLIArgs("-abc foo bar gazonk", "abc:");
+		$this->assertIdentical($args->options, array('a'=>true, 'b'=>true, 'c'=>'foo'));
+		$this->assertIdentical($args->params, array('bar', 'gazonk'));		
+		$this->assertTrue($args->valid);		
+		
+		$args = new CLIArgs("-a -b -c", "abc");
+		$this->assertIdentical($args->options, array('a'=>true, 'b'=>true, 'c'=>true));
+		$this->assertIdentical($args->params, array());	
+		$this->assertTrue($args->valid);			
+		
+		$args = new CLIArgs("-a -b -c", "abc:");
+		$this->assertFalse($args->valid);		
+		
+		$args = new CLIArgs("-abc", "ab:c");
+		$this->assertIdentical($args->options, array('a'=>true, 'b'=>'c'));
+		$this->assertIdentical($args->params, array());			
+		$this->assertTrue($args->valid);
+		
+		$args = new CLIArgs("-abc foo -bar gazonk", "abc");
+		$this->assertIdentical($args->options, array('a'=>true, 'b'=>true, 'c'=>true));
+		$this->assertIdentical($args->params, array('foo', '-bar', 'gazonk'));			
+		$this->assertTrue($args->valid);		
 		
 	}
 	
