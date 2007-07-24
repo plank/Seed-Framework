8e0404e7958c2f2d64d94049c75ec25a03044b16
mateo_at_plank <mateo_at_plank@e74265ba-5712-0410-b544-ed03b9d7f503>
2007-07-24 16:33:15 +0000

    Added phpxbase code.
    git-svn-id: https://seedframework.svn.sourceforge.net/svnroot/seedframework/trunk@473 e74265ba-5712-0410-b544-ed03b9d7f503

---
+++ vendor/phpxbase.php
@@ -0,0 +1,10 @@
+<?php
+
+$dir = "phpxbase-2006-10-11/";
+
+require_once $dir."Column.class.php";
+require_once $dir."Record.class.php";
+require_once $dir."Table.class.php";
+require_once $dir."WritableTable.class.php";
+
+?>
