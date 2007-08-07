1fe202081541cd85288c1e33039d9ca0a4f80e85
mateo_murphy <mateo_murphy@e74265ba-5712-0410-b544-ed03b9d7f503>
2007-08-07 00:53:31 +0000

    Added boundary checks on move higher/lower.
    git-svn-id: https://seedframework.svn.sourceforge.net/svnroot/seedframework/trunk@487 e74265ba-5712-0410-b544-ed03b9d7f503

--- model/lib/list_model.php
+++ model/lib/list_model.php
@@ -21,46 +21,46 @@ class_exists('model') || require('model.php');
  * @package model
  */
 class ListModel extends Model {
-	
+
 	/**
 	 * The field representing the objects position in a list
 	 *
 	 * @var string
 	 */
 	var $position_field = 'position';
-	
+
 	var $scope_condition = '1 = 1';
-	
+
 	/**
-	 * 
+	 *
 	 *
 	 * @return string
 	 */
 	function scope_condition() {
-		return $this->scope_condition;	
-		
+		return $this->scope_condition;
+
 	}
-	
+
 	function insert() {
 
 		if ($this->position_field) {
 			$this->add_to_list_bottom();
 		}
-		
+
 		return parent::insert();
-		
+
 	}
-	
+
 	function delete() {
 		if ($this->position_field) {
-			$this->decrement_position_on_lower_items();	
+			$this->decrement_position_on_lower_items();
 		}
-		
-		
-		return parent::delete();	
-		
+
+
+		return parent::delete();
+
 	}
-	
+
 	/**
 	 * Returns true if the item is in a list
 	 *
@@ -68,13 +68,13 @@ class ListModel extends Model {
 	 */
 	function in_list() {
 		return (isset($this->position_field) && $this->position_field && $this->data[$this->position_field]);
-	
+
 	}
-	
+
 	function remove_from_list() {
-		return $this->decrement_position_on_lower_items();	
+		return $this->decrement_position_on_lower_items();
 	}
-	
+
 	/**
 	 * Moves the item up in the list by swapping its position with
 	 * the one above it
@@ -85,12 +85,15 @@ class ListModel extends Model {
 			trigger_error('Move higher called on an item not in a list', E_USER_WARNING);
 			return false;
 		}
-		
+
 		$item = $this->higher_item();
+
+		if (!$item) return false;
+
 		$item->increment_position();
 		$this->decrement_position();
 	}
-	
+
 	/**
 	 * Moves the item down in the list by swapping its position with
 	 * the one below it
@@ -101,12 +104,15 @@ class ListModel extends Model {
 			trigger_error('Move lower called on an item not in a list', E_USER_WARNING);
 			return false;
 		}
-		
+
 		$item = $this->lower_item();
+
+		if (!$item) return false;
+
 		$this->increment_position();
 		$item->decrement_position();
 	}
-	
+
 	/**
 	 * Increments the position of the current item, moving it down in the list
 	 *
@@ -116,7 +122,7 @@ class ListModel extends Model {
 		$this->data[$this->position_field] ++;
 		return $this->save();
 	}
-	
+
 	/**
 	 * Sets the position value of the object to be after the last item
 	 *
@@ -124,17 +130,17 @@ class ListModel extends Model {
 	function add_to_list_bottom() {
 		$this->data[$this->position_field] = $this->bottom_position_in_list() + 1;
 	}
-	
+
 	/**
 	 * Decrements the position of the current item, moving it up in the list
 	 *
 	 * @return bool
 	 */
 	function decrement_position() {
-		$this->data[$this->position_field] --;		
+		$this->data[$this->position_field] --;
 		$this->save();
 	}
-	
+
 	/**
 	 * Retrieves the item that is higher in the list
 	 *
@@ -145,7 +151,7 @@ class ListModel extends Model {
 		$item = $finder->find('first', array('conditions'=>$this->position_field." = ".($this->data[$this->position_field] - 1)." AND ".$this->scope_condition()));
 		return $item;
 	}
-	
+
 	/**
 	 * Retrieves the item that is lower in the list
 	 *
@@ -155,7 +161,7 @@ class ListModel extends Model {
 		$finder = $this->finder();
 		$item = $finder->find('first', array('conditions'=>$this->position_field." = ".($this->data[$this->position_field] + 1)." AND ".$this->scope_condition()));
 		return $item;
-	
+
 	}
 
 	/**
@@ -167,7 +173,7 @@ class ListModel extends Model {
 		$sql = "SELECT $this->position_field FROM $this->table WHERE ".$this->scope_condition()." ORDER BY $this->position_field DESC LIMIT 1";
 		return $this->db->query_value($sql);
 	}
-	
+
 	/**
 	 * Moves the items below the current item one position
 	 *
@@ -176,13 +182,13 @@ class ListModel extends Model {
 	function decrement_position_on_lower_items() {
 		if ($this->in_list()) {
 			$finder = $this->finder();
-			
+
 			return $finder->update_all("$this->position_field = $this->position_field - 1", $this->position_field." = ".($this->data[$this->position_field] + 1)." AND ".$this->scope_condition());
 		} else {
 			return false;
 		}
 	}
-	
+
 }
 
 ?>
