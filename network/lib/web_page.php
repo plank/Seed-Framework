62e29026a562586ed50fea651b7c411e84ead947
mateo_murphy <mateo_murphy@e74265ba-5712-0410-b544-ed03b9d7f503>
2007-09-11 17:31:38 +0000

    Made some fixes to net library.
    git-svn-id: https://seedframework.svn.sourceforge.net/svnroot/seedframework/trunk@510 e74265ba-5712-0410-b544-ed03b9d7f503

--- network/lib/web_page.php
+++ network/lib/web_page.php
@@ -7,7 +7,7 @@ class WebPage {
 	 * @var URL
 	 */
 	var $url;
-	
+
 	/**
 	 * @var string
 	 */
@@ -20,7 +20,7 @@ class WebPage {
 	 * @var string
 	 */
 	var $content;
-	
+
 	/**
 	 * Opens the page at the given url
 	 *
@@ -29,33 +29,36 @@ class WebPage {
 	 * @return bool
 	 */
 	function open($url, $headers_only = false) {
-		
+
 		if (is_string($url)) {
-			$url = new URL($url);	
+			$url = new URL($url);
 		}
 
 		assert($url);
-		
+
 		$this->url = $url;
-		
+
+		$http = new HTTP(new SimpleSocket());
+		$http->open($url);
+
 		if ($headers_only) {
-			$this->raw = $url->head();	
+			$this->raw = $http->head();
 		} else {
-			$this->raw = $url->get();
+			$this->raw = $http->get();
 		}
 
 		if (!$this->raw) {
-			return false;	
+			return false;
 		}
-		
+
 		list($raw_headers, $this->content) = explode("\r\n\r\n", $this->raw, 2);
-		
+
 		$this->parse_headers($raw_headers);
-		
+
 		return true;
-		
+
 	}
-	
+
 	/**
 	 * Parse http headers
 	 *
@@ -63,49 +66,49 @@ class WebPage {
 	 */
 	function parse_headers($raw_headers) {
 		$headers = explode("\r\n", $raw_headers);
-		
+
 		$this->response_code = array_shift($headers);
-		
+
 		foreach ($headers as $header) {
 			list($key, $value) = explode(': ', $header);
-			
+
 			$this->headers[$key] = $value;
-			
-		}		
-		
+
+		}
+
 	}
-	
+
 	/**
 	 * Returns the title of the document
 	 *
 	 * @return string
 	 */
 	function get_title() {
-		preg_match('@<title>(.*)</title>@is', $this->content, $matches);	
-		
+		preg_match('@<title>(.*)</title>@is', $this->content, $matches);
+
 		if (isset($matches[1])) {
 			return $matches[1];
 		} else {
-			return '';	
+			return '';
 		}
-		
+
 	}
-	
+
 	/**
 	 * Returns the html contents of the document
 	 *
 	 * @return string
 	 */
 	function get_html_contents() {
-		preg_match('@<html>(.*)</html>@is', $this->content, $matches);	
-		
+		preg_match('@<html>(.*)</html>@is', $this->content, $matches);
+
 		if (isset($matches[1])) {
 			return $matches[1];
 		} else {
-			return '';	
+			return '';
 		}
-		
-		
+
+
 	}
 
 	/**
@@ -114,16 +117,16 @@ class WebPage {
 	 * @return string
 	 */
 	function get_base_href() {
-		preg_match("/base[\\s]*href[\\s]*=[\\s]*('|\")([^\"']*)('|\")/is", $this->content, $matches);	
-		
+		preg_match("/base[\\s]*href[\\s]*=[\\s]*('|\")([^\"']*)('|\")/is", $this->content, $matches);
+
 		if (isset($matches[2])) {
 			return $matches[2];
 		} else {
-			return '';	
-		}		
-		
+			return '';
+		}
+
 	}
-	
+
 	/**
 	 * Returns a plain text version of the html contents
 	 *
@@ -134,45 +137,45 @@ class WebPage {
 		$text = $this->remove_tag_and_contents('style', $text);
 		$text = $this->strip_extra_whitespace($text);
 		// remove extraneous whitespace
-		return strip_tags($text);	
-		
+		return strip_tags($text);
+
 	}
-	
+
 	function get_links($url) {
-	   
+
 	   //Pattern building across multiple lines to avoid page distortion.
 	   $pattern  = "/((@import\s+[\"'`]([\w:?=@&\/#._;-]+)[\"'`];)|";
 	   $pattern .= "(:\s*url\s*\([\s\"'`]*([\w:?=@&\/#._;-]+)";
 	   $pattern .= "([\s\"'`]*\))|<[^>]*\s+(src|href|url)\=[\s\"'`]*";
 	   $pattern .= "([\w:?=@&\/#._;-]+)[\s\"'`]*[^>]*>))/i";
-	   
+
 	   preg_match_all ($pattern, $this->content, $matches);
 	   return (is_array($matches)) ? $matches : false;
-	   
+
 	}
-	
+
 	/**
 	 * Returns all the link sources in a piece of text as a string
 	 *
 	 * @param string $text the text to search in
-	 * @param string $must_contain If this is set, only 
+	 * @param string $must_contain If this is set, only
 	 * @return array
 	 */
 	function extract_links($must_contain = '', $mode = 'href') {
 		if (is_array($mode)) {
-			$mode = "(".implode('|', $mode).")";	
+			$mode = "(".implode('|', $mode).")";
 		}
-		
+
 		preg_match_all("/".$mode."[\\s]*=[\\s]*('|\")([^\"']*)('|\")/", $this->content, $matches);
-		
+
 		$results = array();
-		
+
 		$base_href = $this->get_base_href();
-		
+
 		if (!$base_href) {
 			$base_href = $this->url->to_string();
 		}
-		
+
 		foreach($matches[2] as $result) {
 			if (!$must_contain || substr_count($result, $must_contain)) {
 				$url = new URL($result, $base_href);
@@ -181,25 +184,25 @@ class WebPage {
 		}
 	/*
 		if (count($results) == 0 ) {
-			return false;		
-		} 
+			return false;
+		}
 	*/
 		return $results;
-		
+
 	}
-	
+
 	function remove_tag_and_contents($tag, $string) {
 		return preg_replace("'<{$tag}[^>]*>.*</{$tag}>'siU", '', $string);
 	}
-	
+
 	function strip_extra_whitespace($string) {
 		// $string = preg_replace("'\s{1,}'m", ' ', $string);
 		$string = preg_replace("'&nbsp;'m", ' ', $string);
 		return trim(preg_replace("'(\s){1,}'m", ' ', $string));
-		
-		
+
+
 	}
-	
+
 }
 
 ?>
